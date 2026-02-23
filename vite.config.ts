039564f5eb7c4c555551f17f75f574fd276c11fe
Varun Vejalla <varun.vejalla@gmail.com>
2026-02-23 00:42:59 -0500

    changes for deploying to website

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
+﻿import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repoBase = "/polyhedron-topology/";
+
 // eslint-disable-next-line import/no-unused-modules
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === "production" ? repoBase : "/",
   server: {
     port: 5173,
     strictPort: false,
   },
-});
+}));
